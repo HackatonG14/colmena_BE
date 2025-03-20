e2cd61d13a3e4d47defd7108ee752a138bc9f8d3
juan ignacio <nho13@hotmail.com>
2025-03-20 11:21:12 +0100

    feat: Add UserRepository with basic query methods

--- src/main/java/dev/hack14/colmena/repositories/UserRepository.java
+++ src/main/java/dev/hack14/colmena/repositories/UserRepository.java
@@ -1,5 +1,19 @@
 package dev.hack14.colmena.repositories;
 
-public class UserRepositories {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import dev.hack14.colmena.models.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
